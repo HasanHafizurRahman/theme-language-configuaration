5eaee3a89d0c81dc2f78cfd09a84e03d364b8132
HasanHafizurRahman <hasanshanto922@gmail.com>
2024-02-15 11:40:21 +0600

    design modified

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         {children}
-        <ToastContainer />
+        <ToastContainer position="top-right" />
         </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client"
+import Loading from "@/components/Loading";
 import Navbar from "@/components/Navbar";
 import Image from "next/image";
 import { useEffect, useState } from "react";
@@ -27,75 +28,80 @@ export default function Home() {
   const [cartCount, setCartCount] = useState(0);
 
   useEffect(() => {
-    Promise.all([fetchFakeStoreData(), fetchJSONPlaceholderData()])
-      .then(([fakeStoreData, jsonPlaceholderData]) => {
-        setFakeStoreData(fakeStoreData);
-        setJsonPlaceholderData(jsonPlaceholderData);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
+    // Simulate delay using setTimeout
+    const delay = setTimeout(() => {
+      Promise.all([fetchFakeStoreData(), fetchJSONPlaceholderData()])
+        .then(([fakeStoreData, jsonPlaceholderData]) => {
+          setFakeStoreData(fakeStoreData);
+          setJsonPlaceholderData(jsonPlaceholderData);
+          setLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+          setLoading(false);
+        });
+    }, 2000); // Adjust delay time as needed
+
+    return () => clearTimeout(delay); // Cleanup setTimeout on unmount
   }, []);
 
   const handleAddToCart = () => {
     // Increment cart count when user clicks on "Add to Cart" button
     setCartCount((prevCount) => prevCount + 1);
     // Show toast notification
-    toast.success("Item added to cart!", {
-      position: "bottom-right",
-      autoClose: 3000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+    toast.success("Item added to cart!");
   };
 
   return (
-      <>
+    <>
       <div className="mb-4">
-      <Navbar cartCount={cartCount} />
+        <Navbar cartCount={cartCount} />
       </div>
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-{loading ? (
-  <p>Loading...</p>
-) : (
-  <>
-  
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-      {/* Render products from FakeStore API */}
-      {fakeStoreData.map((product) => (
-        <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
-          <Image src={product.image} alt={product.title} width={400} height={200} />
-          <div className="p-4">
-            <h3 className="font-semibold mb-2">{product.title}</h3>
-            <p className="text-primary-34 mb-4 truncate">{product.description}</p>
-            <p className="text-primary-26 font-semibold">{product.price}</p>
-            <button onClick={handleAddToCart} className="mt-4 flex items-center bg-primary-26 text-white px-4 py-2 rounded-md">
+        {loading ? (
+          <Loading />
+        ) : (
+          <>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+              {/* Render products from FakeStore API */}
+              {fakeStoreData.map((product) => (
+                <div key={product.id} className="bg-white rounded-lg shadow-md overflow-hidden">
+                  <Image
+                    src={product.image}
+                    alt={product.title}
+                    width={200}
+                    height={200}
+                    loading="lazy"
+                    className="object-cover mb-2" />
+                  <div className="p-4">
+                    <h3 className="font-semibold mb-2">{product.title}</h3>
+                    <p className="text-primary-34 mb-4 truncate">{product.description}</p>
+                    <p className="text-primary-26 font-semibold">{product.price}</p>
+                    <div className="flex justify-end">
+                    <button onClick={handleAddToCart} className="mt-4 flex items-center bg-primary-26 text-white px-4 py-2 rounded-md">
                       <FaShoppingCart className="mr-2" />
                       Add to Cart
                     </button>
-          </div>
-        </div>
-      ))}
-    </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
 
-    <div className="mt-12">
-      {/* Render posts from JSONPlaceholder API */}
-      {jsonPlaceholderData.map((post) => (
-        <div key={post.id} className="bg-white rounded-lg shadow-md p-6 mb-6">
-          <h3 className="font-semibold mb-2">{post.title}</h3>
-          <p className="text-primary-34">{post.body}</p>
-        </div>
-      ))}
-    </div>
-  </>
-)}
-    </main>
+            <div className="mt-12">
+              {/* Render posts from JSONPlaceholder API */}
+              {jsonPlaceholderData.map((post) => (
+                <div key={post.id} className="bg-white rounded-lg shadow-md p-6 mb-6">
+                  <h3 className="font-semibold mb-2">{post.title}</h3>
+                  <p className="text-primary-34">{post.body}</p>
+                </div>
+              ))}
+            </div>
+          </>
+        )}
+      </main>
     </>
   );
 }
